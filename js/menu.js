a34dd855ebda1d88a2e53f13282c9f9d9f3cc611
Nyolczas <nyolczas@gmail.com>
2019-03-04 20:24:17 +0100

    menu ok

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,20 @@
 var e = document.getElementById('menuMask');
+var t = document.getElementById('taki');
+console.log(t.style.zIndex);
 function change(x) {
     //-- hide light fx
     if(e.style.display == 'none')
         setTimeout(showLightFx, 400);
     else
     e.style.display = 'none';
+
+    console.log(t.style.zIndex);
+    //-- taki z-index
+    if(t.style.zIndex == '') {
+        t.style.zIndex = '99'; 
+    }else {
+        t.style.zIndex = '';
+    }
 
     //-- box anim
     var element = document.getElementById('box1');
